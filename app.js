8243aa5281108fc9732bd4c22d11bdd33d0f1f47
deadfantom2 <cameratest81@gmail.com>
2021-04-01 00:16:03 +0200

    2 commit

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ app.post('/send-about-coin', async (req, res) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-});
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.status(err.status || 500);
+// });
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Mode Production is runnig...');
