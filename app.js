4f1a79098b44fa6414db4b92dbeb0f4b88ab01a1
deadfantom2 <cameratest81@gmail.com>
2021-04-01 02:10:53 +0200

    2 commit

--- app.js
+++ app.js
@@ -57,11 +57,10 @@ app.post('/send-about-coin', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   console.log('Mode Production is runnig...');
 
-  // app.use(express.static(path.join(__dirname, '/build')));
+  app.use(express.static(path.join(__dirname, '/build')));
 
   app.get('*', (req, res) => {
-    console.log('zlekjflke');
-    // res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => res.send('API IS RUNNING...'));
